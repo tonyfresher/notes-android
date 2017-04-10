563f54a417dcaf7ae2143423ef8ce7537cf6e717
Tony Frehser <farnetsohn@gmail.com>
2017-04-10 16:10:51 +0500

    + toasts for view

--- Nowhere/app/src/main/java/com/nowhere/login/CustomView.java
+++ Nowhere/app/src/main/java/com/nowhere/login/CustomView.java
@@ -5,6 +5,9 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -27,5 +30,23 @@ public class CustomView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         Toast.makeText(getContext(), "CustomView.onTouchEvent() called", Toast.LENGTH_SHORT).show();
         return false;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Toast.makeText(getContext(), "CustomView.onMeasure() called", Toast.LENGTH_SHORT).show();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        Toast.makeText(getContext(), "CustomView.onLayout() called", Toast.LENGTH_SHORT).show();
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
+        Toast.makeText(getContext(), "CustomView.onFocusChanged() called", Toast.LENGTH_SHORT).show();
+        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
     }
 }
